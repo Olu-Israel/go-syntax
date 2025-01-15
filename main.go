8e961787b5b833c7253f9c14eacb21e1be16efb8
Olu-israel <olutokijuwon12@gmail.com>
2025-01-14 23:02:39 -0800

    Maps

--- main.go
+++ main.go
@@ -115,16 +115,23 @@ switch color {
 // 	fmt.Printf("Number %d\n", i)
 // }
 
-for i := 1; i < 21; i++ {
-	if i % 3 == 0 {
-		fmt.Printf("Fizz %v ", i)
-	} else if i % 5 == 0 {
-		fmt.Printf("Buzz %v ", i)
-	} else if i % 3 == 0 && i % 5 == 0 {
-		fmt.Printf("FizzBuzz %v", i)
-	}
-	fmt.Println(i)
-}
+// for i := 1; i < 21; i++ {
+// 	if i % 3 == 0 && i % 5 == 0 {
+// 		fmt.Printf("FizzBuzz ")
+// 	} else if i % 3 == 0 {
+// 		fmt.Printf("Fizz ")
+// 	} else if i % 5 == 0 {
+// 		fmt.Printf("Buzz ")
+// 	}
+// 	fmt.Println(i)
+// }
+
+//Maps
+emails := make(map[string]string)
+emails["Bob"] = "bob@gmail.com"
+emails["Sharon"] = "sharon@gmail.com"
+
+fmt.Println(emails["Bob"])
 
 }
 
